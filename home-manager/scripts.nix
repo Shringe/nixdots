83de8fd60aff7bd22cd781e09093dabd288b4745
Shringe <dashingkoso@gmail.com>
2024-10-01 10:47:16 +0000

    added some font and media dependencies for dots

--- home-manager/scripts.nix
+++ home-manager/scripts.nix
@@ -1,6 +1,10 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
+    # media-control
     (writeShellScriptBin "media-control" (builtins.readFile ./scripts/media-control))
+    pulseaudio
+    playerctl
+    libnotify
   ];
 }
