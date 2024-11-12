ba7e2e25affb7756e00e0dc0172ebbb4a8ae4fc8
Shringe <git@nsaria.com>
2024-11-12 00:38:11 -0600

    reverted agenix changes

--- home-manager/homeManagerModules/shells/atuin.nix
+++ home-manager/homeManagerModules/shells/atuin.nix
@@ -1,14 +1,6 @@
-{ lib, config, inputs, ... }:
+{ lib, config, ... }:
 {
-  imports = [
-    inputs.agenix.homeManagerModules.default
-  ];
   programs.atuin = lib.mkIf config.homeManagerModules.shells.atuin.enable {
     enable = true;
-    settings = {
-      # key_path = config.
-      # key_path = config.age.secrets.atuin.path;
-    };
   };
-  age.secrets.atuin.file = ../../../secrets/atuin.age;
 }
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,8 +1,5 @@
 let
   luminum = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHtHOM2G8jkHS54/upxjTWIv/bcjOdPKKgqQLJY5YHfa";
-  # diety = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIjDOkukFCIjZ5cpYKqG5QNFt1P0P5oJaYLUglbxENrb";
-  shringed = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBomNxAq5H1DKnlCQ0lkmWonsoVno+YAyKwUpSxFBn1e";
 in {
   "wireless.age".publicKeys = [ luminum ]; 
-  "atuin.age".publicKeys = [ shringed ];
 }
