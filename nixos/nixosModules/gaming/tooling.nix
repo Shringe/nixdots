918590e4eac0f8f5d9513e09b9d3ca42fccbee70
Shringe <dashingkoso@gmail.com>
2024-12-30 08:25:28 -0600

    general

--- nixos/nixosModules/gaming/tooling.nix
+++ nixos/nixosModules/gaming/tooling.nix
@@ -11,6 +11,25 @@ in
     # pkgs.protonhax
   ];
 
+  nixpkgs.config = lib.mkIf cfg.enable { 
+    packageOverrides = pkgs: {
+      steam = pkgs.steam.override {
+        extraPkgs = pkgs: with pkgs; [
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXinerama
+          xorg.libXScrnSaver
+          libpng
+          libpulseaudio
+          libvorbis
+          stdenv.cc.cc.lib
+          libkrb5
+          keyutils
+        ];
+      };
+    };
+  };
+
   # Temporary: for KDE Plasma's HDR support. Will be removed after universal HDR implementations
   services.desktopManager.plasma6.enable = lib.mkIf cfg.enable true;
 }
