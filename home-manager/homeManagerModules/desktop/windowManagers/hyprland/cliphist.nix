2a00bdd48009ea2a022dd3df304cd9d8ca9c6298
Shringe <git@nsaria.com>
2025-02-07 18:11:27 -0600

    hyprland moved some broken systemd services to exec-once

--- home-manager/homeManagerModules/desktop/windowManagers/hyprland/cliphist.nix
+++ home-manager/homeManagerModules/desktop/windowManagers/hyprland/cliphist.nix
@@ -3,17 +3,20 @@ let
   cfg = config.homeManagerModules.desktop.windowManagers.hyprland.cliphist;
 in
 {
-  home.packages = with pkgs; lib.mkIf cfg.enable [
-    wl-clipboard
-  ];
-
-  services.cliphist = lib.mkIf cfg.enable {
-    enable = true;
-    allowImages = true;
-
-    extraOptions = [
-      "-max-items"
-      "100"
+  config = {
+    home.packages = with pkgs; lib.mkIf cfg.enable [
+      wl-clipboard
+      cliphist
     ];
+
+    services.cliphist = lib.mkIf cfg.enable {
+      # enable = true;
+      allowImages = true;
+
+      extraOptions = [
+        "-max-items"
+        "100"
+      ];
+    };
   };
 }
--- home-manager/homeManagerModules/desktop/windowManagers/hyprland/swaync.nix
+++ home-manager/homeManagerModules/desktop/windowManagers/hyprland/swaync.nix
@@ -1,9 +1,14 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 let
   cfg = config.homeManagerModules.desktop.windowManagers.hyprland.swaync;
 in
 {
-  services.swaync = lib.mkIf cfg.enable {
-    enable = true;
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      swaynotificationcenter
+    ];
   };
+  # services.swaync = lib.mkIf cfg.enable {
+  #   enable = true;
+  # };
 }
