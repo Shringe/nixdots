e92c614c2086e539e5fb62c2266f94c7648a841f
Shringe <git@nsaria.com>
2025-05-19 03:34:40 -0500

    modernized wezterm config to work well with stylix module

--- home-manager/homeManagerModules/desktop/terminals/wezterm/wezterm.lua
+++ home-manager/homeManagerModules/desktop/terminals/wezterm/wezterm.lua
@@ -4,20 +4,17 @@ local config = {}
 config = {
   default_prog = { "/usr/bin/env", "fish" },
 
-  font = wezterm.font("JetBrains Mono"),
-  font_size = 16,
+  -- font = wezterm.font("JetBrains Mono"),
+  -- font_size = 16,
   enable_tab_bar = false,
   -- window_decorations
-  window_background_opacity = 0.8,
-
-  -- fixes rendering issues
-  -- front_end = "WebGpu",
+  -- window_background_opacity = 0.8,
 
   window_padding = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0,
+    left = 2,
+    right = 2,
+    top = 2,
+    bottom = 2,
   },
 }
 
