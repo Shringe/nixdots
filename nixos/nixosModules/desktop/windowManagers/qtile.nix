35ce0d00c0cf8dafa073427b6b4c1fb6fdcefd06
Shringe <dashingkoso@gmail.com>
2025-06-04 21:26:50 -0500

    fix

--- nixos/nixosModules/desktop/windowManagers/qtile.nix
+++ nixos/nixosModules/desktop/windowManagers/qtile.nix
@@ -1,9 +1,9 @@
 { config, lib, ...  }:
 let
-  cfg = config.nixosModules.desktop.qtile;
+  cfg = config.nixosModules.desktop.windowManagers.qtile;
 in
 {
-  services.xserver.windowManager.qtile = {
+  services.xserver.windowManager.qtile = lib.mkIf cfg.enable {
     enable = true;
     extraPackages = python3Packages: with python3Packages; [
       qtile-extras
