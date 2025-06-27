8de291d1dd95a672abfb017c125b8345461b0607
Shringe <git@nsaria.com>
2025-06-26 20:56:05 -0500

    cleaned fish

--- home-manager/homeManagerModules/shells/fish.nix
+++ home-manager/homeManagerModules/shells/fish.nix
@@ -1,28 +1,29 @@
 { lib, pkgs, config, ... }:
-{
-  config.programs = {
-    # atuin = lib.mkIf config.shells.fish.atuin { 
-    #   enableFishIntegration = true;
-    # };
-    fish = lib.mkIf config.homeManagerModules.shells.fish.enable {
-      enable = true;
-      interactiveShellInit = ''
-        fish_vi_key_bindings
-        set fish_greeting # disable greeting
-      '';
+with lib;
+let
+  cfg = config.homeManagerModules.shells.fish;
+in {
+  config = mkIf cfg.enable {
+    programs = {
+      atuin.enableFishIntegration = true;
+      yazi.enableFishIntegration = true;
 
-      plugins = [
-        # { name = "transient-fish" ; src = pkgs.fishPlugins.transient-fish.src; }  
-        # { name = "bobthefish" ; src = pkgs.fishPlugins.bobthefish.src; }  
-        # { name = "hydro" ; src = pkgs.fishPlugins.hydro.src; }  
-        # { name = "tide" ; src = pkgs.fishPlugins.tide.src; }  
-        # { name = "pure" ; src = pkgs.fishPlugins.pure.src; }
-        # { name = "done" ; src = pkgs.fishPlugins.done.src; }
-        { name = "colored-man-pages" ; src = pkgs.fishPlugins.colored-man-pages.src; }
-        { name = "pisces" ; src = pkgs.fishPlugins.pisces.src; }
-        { name = "fzf-fish" ; src = pkgs.fishPlugins.fzf-fish.src; }
-        { name = "humantime-fish" ; src = pkgs.fishPlugins.humantime-fish.src; }
-      ];
+      fish = {
+        enable = true;
+        interactiveShellInit = ''
+          fish_vi_key_bindings
+          set fish_greeting
+        '';
+
+        plugins = [
+          { name = "tide" ; src = pkgs.fishPlugins.tide.src; }  
+          { name = "done" ; src = pkgs.fishPlugins.done.src; }
+          { name = "colored-man-pages" ; src = pkgs.fishPlugins.colored-man-pages.src; }
+          { name = "pisces" ; src = pkgs.fishPlugins.pisces.src; }
+          { name = "fzf-fish" ; src = pkgs.fishPlugins.fzf-fish.src; }
+          { name = "humantime-fish" ; src = pkgs.fishPlugins.humantime-fish.src; }
+        ];
+      };
     };
   };  
 }
