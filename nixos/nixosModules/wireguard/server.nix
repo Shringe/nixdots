619e31e4be0ddfc3a4a5e5ec4ea4c2c1aaf26ac1
Shringe <git@nsaria.com>
2025-07-01 14:31:44 -0500

    functional wgnew

--- nixos/nixosModules/wireguard/server.nix
+++ nixos/nixosModules/wireguard/server.nix
@@ -1,4 +1,5 @@
 { config, lib, pkgs, ... }:
+
 with lib;
 let
   cfg = config.nixosModules.wireguard.server;
@@ -25,43 +26,22 @@ in
   config = mkIf cfg.enable {
     sops.secrets."wireguard/server" = {};
     environment.systemPackages = with pkgs; [
-      # Example:
-      # [Interface]
-      # PrivateKey = WGpL3/ejM5L9ngLoAtXkSP1QTNp4eSD34Zh6/Jfni1Q=
-      # ListenPort = 51820
-      # DNS = 10.6.210.1, pfSense.home.arpa
-      # Address = 10.6.210.2/24
-      #
-      # [Peer]
-      # PublicKey = PUVBJ+zuz/0mRPEB4tIaVbet5NzVwdWMX7crGx+/wDs=
-      # AllowedIPs = 0.0.0.0/0
-      # Endpoint = 198.51.100.6:51820
-
-      (writers.writeFishBin "wgnew" ''
-        # set server_private_key (cat "${config.sops.secrets."wireguard/server".path}") 
-        # set server_private_key ""
-
-        function is_debug_mode
-          if test "$argv[1]" = "--debug"
-            true
-          else
-            false
-          end
-        end
-
-        echo (is_debug_mode $argv)
-      '')
-
       (writeShellApplication {
-        name = "wgnew-sh";
+        name = "wgnew";
         runtimeInputs = [
           openssl
           wireguard-tools
+          fish
         ];
 
         text = ''
-          # serverPrivateKey
-          echo hello!
+          fish ${./wgnew.fish} \
+            ${config.sops.secrets."wireguard/server".path} \
+            ${config.nixosModules.info.system.ips.local} \
+            "${cfg.private_ip}.$1" \
+            "${config.nixosModules.info.system.ips.public}:${toString cfg.port}" \
+            "0.0.0.0/0" \
+            "$2" # Name
         '';
       })
     ];
@@ -90,27 +70,23 @@ in
         privateKeyFile = config.sops.secrets."wireguard/server".path;
 
         peers = [
-          { # My phone
-            # publicKey = "ltDO0r/WvwS8fnNa+zyiK5xEa1ZqSHLvtyrkIiUubn4=";
-            publicKey = "Ferj3XiXDaAh3BAOznxIx7iVF+3MAjYfutPYJF9gGmA=";
-            allowedIPs = [ "${cfg.private_ip}.2/32" ];
-          }
+          # { # My phone
+          #   # publicKey = "ltDO0r/WvwS8fnNa+zyiK5xEa1ZqSHLvtyrkIiUubn4=";
+          #   publicKey = "Ferj3XiXDaAh3BAOznxIx7iVF+3MAjYfutPYJF9gGmA=";
+          #   allowedIPs = [ "${cfg.private_ip}.2/32" ];
+          # }
           { # K
             publicKey = "3HSS1loEZSCGjfqOLm/dyZpPfwzT31wKvw8Ygrl5czE=";
             allowedIPs = [ "${cfg.private_ip}.3/32" ];
-          }
-          { # Sam PC
-            publicKey = "7V6HsDx5pYjQMht9Wq2uFZuw3OPfICZIiIPcfhcyM3U=";
-            allowedIPs = [ "${cfg.private_ip}.4/32" ];
           }
           { # My laptop
             publicKey = "D3qbFvDBUdQ1jJhvS2zUhiyi7hxikS4Y0PgLYUYkp3I=";
             allowedIPs = [ "${cfg.private_ip}.5/32" ];
           }
-          # { # Preshared my phone
-          #   publicKey = 
-          #   allowedIPs = [ "${cfg.private_ip}.6/32" ];
-          # }
+          { # New Phone
+            publicKey = "kxDYZxPPhAQhgdd3u+/G0eJCiWhALGxmdKndjxTZXnw=";
+            allowedIPs = [ "10.100.0.6/32" ];
+          }
         ];
       };
     };
