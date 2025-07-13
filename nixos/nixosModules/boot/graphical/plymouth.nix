4b2a931284ab40f250976f046538683f9c18764a
Shringe <dashingkoso@gmail.com>
2025-07-12 23:13:27 -0500

    general

--- nixos/nixosModules/boot/graphical/plymouth.nix
+++ nixos/nixosModules/boot/graphical/plymouth.nix
@@ -6,11 +6,41 @@ in {
   options.nixosModules.boot.graphical.plymouth = {
     enable = mkOption {
       type = types.bool;
-      default = config.nixosModules.boot.graphical.enable;
+      # default = config.nixosModules.boot.graphical.enable;
+      default = false;
     };
   };
 
   config = mkIf cfg.enable {
+    boot = {
+      # For clean nvidia boot
+      uvesafb = {
+        enable = true;
+        gfx-mode = "3440x1440@175";
+      };
 
+      # Enable "Silent boot"
+      consoleLogLevel = 3;
+      initrd.verbose = false;
+      kernelParams = [
+        "quiet"
+        "splash"
+        "boot.shell_on_fail"
+        "udev.log_priority=3"
+        "rd.systemd.show_status=auto"
+      ];
+
+      plymouth = {
+        enable = true;
+
+        # theme = "rings";
+        # themePackages = with pkgs; [
+        #   # By default we would install all themes
+        #   (adi1090x-plymouth-themes.override {
+        #     selected_themes = [ "rings" ];
+        #   })
+        # ];
+      };
+    };
   };
 }
