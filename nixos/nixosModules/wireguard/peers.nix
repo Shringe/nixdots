31aa684e44693d6ee5d4153851c1d3fabb331768
Shringe <git@nsaria.com>
2025-07-13 19:24:47 -0500

    added wireguard peers

--- nixos/nixosModules/wireguard/peers.nix
+++ nixos/nixosModules/wireguard/peers.nix
@@ -28,14 +28,18 @@ in {
   config = mkIf cfg.enable {
     sops.secrets = {
       "preshared/L_Phone" = key;
+      "preshared/K_Phone" = key;
+      "preshared/K_Laptop" = key;
     };
 
     networking.wireguard.interfaces.wg0.peers = [
-      { # K Phone
-        publicKey = "3HSS1loEZSCGjfqOLm/dyZpPfwzT31wKvw8Ygrl5czE=";
-        allowedIPs = [ "${cfg.private_ip}.3/32" ];
-      }
+      # { # K Phone
+      #   publicKey = "3HSS1loEZSCGjfqOLm/dyZpPfwzT31wKvw8Ygrl5czE=";
+      #   allowedIPs = [ "${cfg.private_ip}.3/32" ];
+      # }
       (mkPeer 2 "L_Phone" "5PfQalJfIoZwTCD7pjamN1PjsqC4V7wWfg0M1kIvqUo=")
+      (mkPeer 3 "K_Phone" "XbbfzMbue9NF7QjJpi68w3SciQJGZq954X9aZtoXb0s=")
+      (mkPeer 4 "K_Laptop" "t+CZw/W6/7G8PTxUiYn9C6lqwak1OFfTRvGU0YTb9UM=")
     ];
   };
 }
