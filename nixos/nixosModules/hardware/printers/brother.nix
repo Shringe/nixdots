a4af1bd7523a051b1484d0930891ea3042344050
Shringe <dashingkoso@gmail.com>
2025-07-19 13:57:22 -0500

    add brlaser and rename

--- nixos/nixosModules/hardware/printers/brother.nix
+++ nixos/nixosModules/hardware/printers/brother.nix
@@ -1,9 +1,9 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 with lib;
 let
-  cfg = config.nixosModules.hardware.printers.brotherhl2280dw;
+  cfg = config.nixosModules.hardware.printers.brother;
 in {
-  options.nixosModules.hardware.printers.brotherhl2280dw = {
+  options.nixosModules.hardware.printers.brother = {
     enable = mkOption {
       type = types.bool;
       default = config.nixosModules.hardware.printers.enable;
@@ -13,6 +13,8 @@ in {
   config = mkIf cfg.enable {
     services.printing = {
       enable = true;
+
+      drivers = [ pkgs.brlaser ];
     };
   };
 }
