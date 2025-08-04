05aa01d69bf9e3828ae65aa92c957a5a51d0c228
Shringe <dashingkoso@gmail.com>
2025-08-04 03:39:11 -0500

    root done

--- nixos/nixosModules/drives/root.nix
+++ nixos/nixosModules/drives/root.nix
@@ -5,21 +5,25 @@
       "d /mnt/btr 0755 root root -"
       "d /mnt/btr/pool 0755 root root -"
       "d /mnt/btr/backups 0755 root root -"
+
+      "d /mnt/btr/pool/root 0755 root root -"
     ];
 
     fileSystems = {
-      "/defvol/root" = {
+      "/mnt/btr/pool/root" = {
         device = "/dev/disk/by-label/nixos";
         fsType = "btrfs";
         options = [ "noatime" "compress=zstd" ];
       };
     };
 
-    services.btrbk.instances."daily".settings.volume."/defvol/root" = lib.mkIf config.nixosModules.backups.btrbk.enable {
-      subvolume = {
-        "_active/@" = {};
-        "_active/@home" = {};
-        "_active/log" = {};
+    services.btrbk.instances = {
+      "daily".settings.volume."/mnt/btr/pool/root" = lib.mkIf config.nixosModules.backups.btrbk.enable {
+        subvolume = {
+          "_active/@" = {};
+          "_active/@home" = {};
+          "_active/log" = {};
+        };
       };
     };
   };
