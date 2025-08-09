4b66a4e3f57fe73edc2f7be4f8b2a72d7c33dd2e
Shringe <git@nsaria.com>
2025-08-09 17:02:39 -0500

    fixed plugin packing for massive statuptime reduction

--- home-manager/homeManagerModules/nixvim/optimizations.nix
+++ home-manager/homeManagerModules/nixvim/optimizations.nix
@@ -1,9 +1,14 @@
-{ pkgs, config, lib, ... }:
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
 {
   home.packages = [ pkgs.vim-startuptime ];
 
   # Tested with:
-  # time vim-startuptime -vimpath nvim 
+  # time vim-startuptime -vimpath nvim
   programs.nixvim.performance = lib.mkIf config.homeManagerModules.nixvim.optimizations {
     # ~25% speed up
     byteCompileLua = {
@@ -14,11 +19,14 @@
       plugins = true;
     };
 
-    # Currently shows ~12ms decrease with 5 plugins 
+    # Currently shows ~12ms decrease with 5 plugins
+    # 2025-08-09, I have more plugins now and it is almost a 3x speedup
     combinePlugins = {
-      # enable = true; # Currently with on blink-cmp
+      enable = true;
       standalonePlugins = [
         "nvim-treesitter"
+        "blink.cmp"
+        "codecompanion.nvim"
       ];
     };
   };
