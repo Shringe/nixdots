9a21e11b75f074af4e49a169aed531e37b088025
Shringe <git@nsaria.com>
2025-08-09 20:27:25 -0500

    switched to deepseek, added keybinds

--- home-manager/homeManagerModules/nixvim/extra-plugins/codecompanion.nix
+++ home-manager/homeManagerModules/nixvim/extra-plugins/codecompanion.nix
@@ -1,54 +1,84 @@
 let
   domain = "https://ollama.deamicis.top";
+  # model = "qwen2.5-coder:latest";
+  model = "deepseek-r1:latest";
 in
 {
-  programs.nixvim.plugins.codecompanion = {
-    enable = false;
+  programs.nixvim = {
+    plugins.codecompanion = {
+      enable = true;
 
-    settings = {
-      adapters = {
-        ollama = {
-          __raw = ''
-            function()
-              return require('codecompanion.adapters').extend('ollama', {
-                  env = {
-                      url = "${domain}",
-                  },
-                  schema = {
-                      model = {
-                          default = 'qwen2.5-coder:latest',
-                          -- default = "llama3.1:8b-instruct-q8_0",
-                      },
-                      num_ctx = {
-                          default = 32768,
-                      },
-                  },
-              })
-            end
-          '';
-        };
-      };
-
-      opts = {
-        log_level = "TRACE";
-        send_code = true;
-        use_default_actions = true;
-        use_default_prompts = true;
-      };
-
-      strategies = {
-        agent = {
-          adapter = "ollama";
+      settings = {
+        adapters = {
+          ollama = {
+            __raw = ''
+              function()
+                return require("codecompanion.adapters").extend("ollama", {
+                    env = {
+                        url = "${domain}",
+                    },
+                    schema = {
+                        model = {
+                            default = "${model}",
+                        },
+                        num_ctx = {
+                            default = 32768,
+                        },
+                    },
+                })
+              end
+            '';
+          };
         };
 
-        chat = {
-          adapter = "ollama";
+        opts = {
+          log_level = "TRACE";
+          send_code = true;
+          use_default_actions = true;
+          use_default_prompts = true;
         };
 
-        inline = {
-          adapter = "ollama";
+        strategies = {
+          agent = {
+            adapter = "ollama";
+          };
+
+          chat = {
+            adapter = "ollama";
+          };
+
+          inline = {
+            adapter = "ollama";
+          };
         };
       };
     };
+
+    keymaps = [
+      {
+        mode = [
+          "n"
+          "v"
+        ];
+        key = "<leader>ss";
+        action = "<cmd>CodeCompanion<CR>";
+      }
+      {
+        mode = [
+          "n"
+          "v"
+        ];
+        key = "<leader>sa";
+        action = "<cmd>CodeCompanionActions<CR>";
+      }
+      {
+        mode = [
+          "n"
+          "v"
+        ];
+        key = "<leader>st";
+        action = "<cmd>CodeCompanionChat<CR>";
+      }
+    ];
   };
 }
