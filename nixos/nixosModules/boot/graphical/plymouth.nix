1e5d716933ae52aaa6d9362e04c46e2db77645c7
Shringe <dashingkoso@gmail.com>
2025-09-03 18:48:44 -0500

    plymouth on luks prompt

--- nixos/nixosModules/boot/graphical/plymouth.nix
+++ nixos/nixosModules/boot/graphical/plymouth.nix
@@ -16,10 +16,15 @@ in
     boot = {
       # Enable "Silent boot"
       consoleLogLevel = 3;
-      initrd.verbose = false;
+      initrd = {
+        verbose = false;
+        systemd.enable = true;
+      };
+
       kernelParams = [
         "quiet"
         "splash"
+        "intremap=on"
         "boot.shell_on_fail"
         "udev.log_priority=3"
         "rd.systemd.show_status=auto"
