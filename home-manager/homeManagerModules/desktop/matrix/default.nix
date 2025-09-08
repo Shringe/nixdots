d1b0486ac49de368499f67a03bbc5f5cb415929b
Shringe <dashingkoso@gmail.com>
2025-09-08 16:10:10 -0500

    added nheko

--- home-manager/homeManagerModules/desktop/matrix/default.nix
+++ home-manager/homeManagerModules/desktop/matrix/default.nix
@@ -2,22 +2,19 @@
 with lib;
 let
   cfg = config.homeManagerModules.desktop.matrix;
-in {
+in
+{
   options.homeManagerModules.desktop.matrix = {
     enable = mkOption {
       type = types.bool;
-      default = config.homeManagerModules.desktop.enable; 
+      default = config.homeManagerModules.desktop.enable;
     };
   };
 
   config = mkIf cfg.enable {
-    sops.secrets."desktop/matrix/iamb" = {
-      path = "${config.home.homeDirectory}/.config/iamb/config.toml";
-    };
-
     programs = {
       element-desktop.enable = true;
-      iamb.enable = true;
+      nheko.enable = true;
     };
   };
 }
