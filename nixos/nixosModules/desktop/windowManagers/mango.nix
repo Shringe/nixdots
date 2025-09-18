15f9c550f0dbf8587d68f02f12805f56208b34f9
Shringe <git@nsaria.com>
2025-09-18 17:27:01 -0500

    add nixos module

---
+++ nixos/nixosModules/desktop/windowManagers/mango.nix
@@ -0,0 +1,50 @@
+{
+  inputs,
+  config,
+  lib,
+  ...
+}:
+with lib;
+let
+  cfg = config.nixosModules.desktop.windowManagers.mango;
+in
+{
+  imports = [
+    inputs.mango.nixosModules.mango
+  ];
+
+  options.nixosModules.desktop.windowManagers.mango = {
+    enable = mkOption {
+      type = types.bool;
+      default = config.nixosModules.desktop.windowManagers.enable;
+    };
+  };
+
+  config = mkIf cfg.enable {
+    sessionVariables = {
+      NIXOS_OZONE_WL = "1";
+      WLR_RENDERER = "vulkan";
+    };
+  };
+
+  security = {
+    pam.services.hyprlock = { };
+    rtkit.enable = true; # For real time audio
+  };
+
+  programs.mango.enable = true;
+
+  # xdg.portal = {
+  #   enable = true;
+  #   config.common.default = [ "wlr" ];
+  #   xdgOpenUsePortal = true;
+  #
+  #   extraPortals = with pkgs; [
+  #     xdg-desktop-portal-wlr
+  #   ];
+  # };
+
+  # services.displayManager.sessionPackages = optional cfg.enable pkgs.mango;
+  # services.gnome.gnome-keyring.enable = true;
+
+}
