0ccd6ab9e87c853c6140acb46090a2b02c89d278
Shringe <git@nsaria.com>
2025-09-18 21:24:11 -0500

    updated configuration

--- home-manager/homeManagerModules/desktop/windowManagers/mango/default.nix
+++ home-manager/homeManagerModules/desktop/windowManagers/mango/default.nix
@@ -28,26 +28,26 @@ in
       # More option see https://github.com/DreamMaoMao/mango/wiki/
       settings = ''
         # Window effect
-        blur=0
+        blur=1
         blur_layer=0
         blur_optimized=1
-        blur_params_num_passes = 2
-        blur_params_radius = 5
-        blur_params_noise = 0.02
-        blur_params_brightness = 0.9
-        blur_params_contrast = 0.9
-        blur_params_saturation = 1.2
+        blur_params_num_passes = 3
+        # blur_params_radius = 5
+        # blur_params_noise = 0.02
+        # blur_params_brightness = 0.9
+        # blur_params_contrast = 0.9
+        # blur_params_saturation = 1.1
 
-        shadows = 0
-        layer_shadows = 0
+        shadows = 1
+        layer_shadows = 1
         shadow_only_floating = 1
-        shadows_size = 10
-        shadows_blur = 15
+        shadows_size = 5
+        shadows_blur = 10
         shadows_position_x = 0
         shadows_position_y = 0
-        shadowscolor= 0x000000ff
+        shadowscolor= 0x45475aff
 
-        border_radius=6
+        border_radius=8
         no_radius_when_single=0
         focused_opacity=1.0
         unfocused_opacity=1.0
@@ -65,10 +65,10 @@ in
         zoom_end_ratio=0.8
         fadein_begin_opacity=0.5
         fadeout_begin_opacity=0.8
-        animation_duration_move=500
-        animation_duration_open=400
-        animation_duration_tag=350
-        animation_duration_close=800
+        animation_duration_move=400
+        animation_duration_open=320
+        animation_duration_tag=280
+        animation_duration_close=600
         animation_curve_open=0.46,1.0,0.29,1
         animation_curve_move=0.46,1.0,0.29,1
         animation_curve_tag=0.46,1.0,0.29,1
@@ -111,8 +111,8 @@ in
         drag_tile_to_tile=1
 
         # keyboard
-        repeat_rate=25
-        repeat_delay=600
+        repeat_rate=35
+        repeat_delay=500
         numlockon=1
         xkb_rules_layout=us
 
@@ -126,25 +126,31 @@ in
         disable_while_typing=1
         left_handed=0
         middle_button_emulation=0
-        swipe_min_threshold=1
+        swipe_min_threshold=0
 
         # mouse
-        # need relogin to make it apply
         mouse_natural_scrolling=0
+        accel_profile=1
+        accel_speed=-0.675
+        adaptive_sync=1
+
+        # Display
+        monitorrule=DP-1,0.55,1,tile,0,1,0,0,2560,1440,165
+        monitorrule=HDMI-A-1,0.55,1,tile,0,1,2560,0,3440,1440,175
 
         # Appearance
-        gappih=5
-        gappiv=5
-        gappoh=10
-        gappov=10
+        gappih=4
+        gappiv=4
+        gappoh=4
+        gappov=4
         scratchpad_width_ratio=0.8
         scratchpad_height_ratio=0.9
-        borderpx=4
-        rootcolor=0x201b14ff
-        bordercolor=0x444444ff
-        focuscolor=0xc9b890ff
+        borderpx=2
+        rootcolor=0x000000ff
+        bordercolor=0x45475aff
+        focuscolor=0x89b4faff
         maxmizescreencolor=0x89aa61ff
-        urgentcolor=0xad401fff
+        urgentcolor=0xf38ba8ff
         scratchpadcolor=0x516c93ff
         globalcolor=0xb153a7ff
         overlaycolor=0x14a57cff
@@ -167,129 +173,131 @@ in
         # mod keys name: super,ctrl,alt,shift,none
 
         # reload config
-        bind=SUPER,r,reload_config
+        bind=SUPER+Ctrl,r,reload_config
 
-        # menu and terminal
-        bind=Super,s,spawn,wofi --show drun
-        bind=Super,Return,spawn,wezterm
+        # Screenshots
+        bind=none,Print,spawn_shell,grim "$HOME/Pictures/screenshots/dwl/$(date +%Y-%m-%d_%H-%m-%s).png"
+        bind=shift,Print,spawn_shell,grim -g "$(slurp)" "$HOME/Pictures/screenshots/dwl/$(date +%Y-%m-%d_%H-%m-%s).png"
 
-        # exit
-        bind=SUPER+Shift,q,quit
-        bind=Super,w,killclient,
+        # Menus
+        bind=SUPER,s,spawn,wofi --show drun
+        bind=SUPER,x,spawn,wlogout
+        bind=SUPER,c,spawn,wofi-emoji
+        bind=SUPER,d,spawn_shell,cliphist list | wofi --dmenu | cliphist decode | wl-copy
 
-        # switch window focus
-        bind=SUPER,Tab,focusstack,next
-        bind=ALT,Left,focusdir,left
-        bind=ALT,Right,focusdir,right
-        bind=ALT,Up,focusdir,up
-        bind=ALT,Down,focusdir,down
+        # Apps
+        bind=SUPER,a,spawn,joplin-desktop
+        bind=SUPER,r,spawn,zen
+        bind=SUPER,Return,spawn,wezterm
 
-        # swap window
-        bind=SUPER+SHIFT,Up,exchange_client,up
-        bind=SUPER+SHIFT,Down,exchange_client,down
-        bind=SUPER+SHIFT,Left,exchange_client,left
-        bind=SUPER+SHIFT,Right,exchange_client,right
+        # Window management - DWL style
+        bind=SUPER,e,focusstack,next
+        bind=SUPER,i,focusstack,prev
+        bind=SUPER,n,incnmaster,1
+        bind=SUPER,o,incnmaster,-1
+        bind=SUPER,l,setmfact,-0.05
+        bind=SUPER,u,setmfact,+0.05
+        bind=SUPER,y,zoom
+        bind=SUPER,Tab,view,last
+        bind=SUPER,w,killclient
 
-        # switch window status
-        bind=SUPER,g,toggleglobal,
-        bind=ALT,Tab,toggleoverview,
-        bind=ALT,backslash,togglefloating,
-        bind=ALT,a,togglemaxmizescreen,
-        bind=ALT,f,togglefullscreen,
-        bind=ALT+SHIFT,f,togglefakefullscreen,
-        bind=SUPER,i,minimized,
-        bind=SUPER,o,toggleoverlay,
-        bind=SUPER+SHIFT,I,restore_minimized
-        bind=ALT,z,toggle_scratchpad
+        # Layout switching - DWL style
+        bind=SUPER,j,setlayout,tile
+        bind=SUPER,m,setlayout,scroller
+        bind=SUPER,k,setlayout,monocle
+        bind=SUPER,h,setlayout,spiral
+        bind=SUPER,space,switch_layout
 
-        # scroller layout
-        bind=ALT,e,set_proportion,1.0
-        bind=ALT,x,switch_proportion_preset,
+        # Window states
+        bind=SUPER+SHIFT,space,togglefloating
+        bind=SUPER,t,togglefullscreen
+        bind=SUPER,F5,togglefakefullscreen
+        bind=SUPER,g,togglegaps
 
-        # switch layout
-        bind=SUPER,n,switch_layout
+        # Tag operations
+        bind=SUPER,1,view,1
+        bind=SUPER,2,view,2
+        bind=SUPER,3,view,3
+        bind=SUPER,4,view,4
+        bind=SUPER,5,view,5
+        bind=SUPER,6,view,6
+        bind=SUPER,7,view,7
+        bind=SUPER,8,view,8
+        bind=SUPER,9,view,9
+        bind=SUPER,0,view,all
 
-        # tag switch
-        bind=SUPER,Left,viewtoleft,
-        bind=CTRL,Left,viewtoleft_have_client,
-        bind=SUPER,Right,viewtoright,
-        bind=CTRL,Right,viewtoright_have_client,
-        bind=CTRL+SUPER,Left,tagtoleft,
-        bind=CTRL+SUPER,Right,tagtoright,
+        bind=SUPER+SHIFT,exclam,tag,1
+        bind=SUPER+SHIFT,at,tag,2
+        bind=SUPER+SHIFT,numbersign,tag,3
+        bind=SUPER+SHIFT,dollar,tag,4
+        bind=SUPER+SHIFT,percent,tag,5
+        bind=SUPER+SHIFT,asciicircum,tag,6
+        bind=SUPER+SHIFT,ampersand,tag,7
+        bind=SUPER+SHIFT,asterisk,tag,8
+        bind=SUPER+SHIFT,parenleft,tag,9
+        bind=SUPER+SHIFT,parenright,tag,all
 
-        bind=Ctrl,1,view,1
-        bind=Ctrl,2,view,2
-        bind=Ctrl,3,view,3
-        bind=Ctrl,4,view,4
-        bind=Ctrl,5,view,5
-        bind=Ctrl,6,view,6
-        bind=Ctrl,7,view,7
-        bind=Ctrl,8,view,8
-        bind=Ctrl,9,view,9
+        bind=SUPER+CTRL,1,toggleview,1
+        bind=SUPER+CTRL,2,toggleview,2
+        bind=SUPER+CTRL,3,toggleview,3
+        bind=SUPER+CTRL,4,toggleview,4
+        bind=SUPER+CTRL,5,toggleview,5
+        bind=SUPER+CTRL,6,toggleview,6
+        bind=SUPER+CTRL,7,toggleview,7
+        bind=SUPER+CTRL,8,toggleview,8
+        bind=SUPER+CTRL,9,toggleview,9
 
-        # tag: move client to the tag and focus it
-        # tagsilent: move client to the tag and not focus it
-        # bind=Alt,1,tagsilent,1
-        bind=Alt,1,tag,1
-        bind=Alt,2,tag,2
-        bind=Alt,3,tag,3
-        bind=Alt,4,tag,4
-        bind=Alt,5,tag,5
-        bind=Alt,6,tag,6
-        bind=Alt,7,tag,7
-        bind=Alt,8,tag,8
-        bind=Alt,9,tag,9
+        bind=SUPER+CTRL+SHIFT,exclam,toggletag,1
+        bind=SUPER+CTRL+SHIFT,at,toggletag,2
+        bind=SUPER+CTRL+SHIFT,numbersign,toggletag,3
+        bind=SUPER+CTRL+SHIFT,dollar,toggletag,4
+        bind=SUPER+CTRL+SHIFT,percent,toggletag,5
+        bind=SUPER+CTRL+SHIFT,asciicircum,toggletag,6
+        bind=SUPER+CTRL+SHIFT,ampersand,toggletag,7
+        bind=SUPER+CTRL+SHIFT,asterisk,toggletag,8
+        bind=SUPER+CTRL+SHIFT,parenleft,toggletag,9
 
-        # monitor switch
-        bind=alt+shift,Left,focusmon,left
-        bind=alt+shift,Right,focusmon,right
-        bind=SUPER+Alt,Left,tagmon,left
-        bind=SUPER+Alt,Right,tagmon,right
+        # Monitor operations
+        bind=SUPER,comma,focusmon,left
+        bind=SUPER,period,focusmon,right
+        bind=SUPER+SHIFT,less,tagmon,left
+        bind=SUPER+SHIFT,greater,tagmon,right
 
-        # gaps
-        bind=ALT+SHIFT,X,incgaps,1
-        bind=ALT+SHIFT,Z,incgaps,-1
-        bind=ALT+SHIFT,R,togglegaps
+        # Media Keys (from DWL SHARED_KEYS)
+        bind=none,XF86AudioRaiseVolume,spawn,swayosd-client --output-volume raise
+        bind=none,XF86AudioLowerVolume,spawn,swayosd-client --output-volume lower
+        bind=none,XF86AudioMute,spawn,swayosd-client --output-volume mute-toggle
+        bind=CTRL,XF86AudioRaiseVolume,spawn,swayosd-client --input-volume raise
+        bind=CTRL,XF86AudioLowerVolume,spawn,swayosd-client --input-volume lower
+        bind=CTRL,XF86AudioMute,spawn,swayosd-client --input-volume mute-toggle
+        bind=none,XF86MonBrightnessUp,spawn,swayosd-client --brightness raise
+        bind=none,XF86MonBrightnessDown,spawn,swayosd-client --brightness lower
+        bind=SHIFT,XF86MonBrightnessUp,spawn,playerctl shuffle Toggle
+        bind=SHIFT,XF86MonBrightnessDown,spawn,playerctl loop Track
+        bind=CTRL,XF86MonBrightnessUp,spawn,playerctl loop None
+        bind=CTRL,XF86MonBrightnessDown,spawn,playerctl loop Playlist
+        bind=SHIFT,XF86AudioRaiseVolume,spawn,playerctl volume 0.1+
+        bind=SHIFT,XF86AudioLowerVolume,spawn,playerctl volume 0.1-
+        bind=SHIFT,XF86AudioMute,spawn,playerctl volume 0
+        bind=none,XF86AudioPlay,spawn,playerctl play-pause
+        bind=none,XF86AudioNext,spawn,playerctl next
+        bind=none,XF86AudioPrev,spawn,playerctl previous
 
-        # adjust tile window size
-        # change master fact for tile,spiral,deck,dwindle
-        bind=ALT+SUPER,h,setmfact,-0.05
-        bind=ALT+SUPER,l,setmfact,+0.05
-        # change sub master fact for dwindle,spiral
-        bind=ALT+SUPER,k,setsmfact,-0.05
-        bind=ALT+SUPER,j,setsmfact,+0.05
-        # change scroller proportion
-        bind=ctrl+super,j,increase_proportion,0.1
-        bind=ctrl+super,k,increase_proportion,-0.1
+        # System controls
+        bind=SUPER+CTRL,q,quit
 
-        # movewin
-        bind=CTRL+SHIFT,Up,movewin,+0,-50
-        bind=CTRL+SHIFT,Down,movewin,+0,+50
-        bind=CTRL+SHIFT,Left,movewin,-50,+0
-        bind=CTRL+SHIFT,Right,movewin,+50,+0
-
-        # resizewin
-        bind=CTRL+ALT,Up,resizewin,+0,-50
-        bind=CTRL+ALT,Down,resizewin,+0,+50
-        bind=CTRL+ALT,Left,resizewin,-50,+0
-        bind=CTRL+ALT,Right,resizewin,+50,+0
-
-        # Mouse Button Bindings
-        # NONE mode key only work in ov mode
+        # Mouse Button Bindings (adapted from DWL)
         mousebind=SUPER,btn_left,moveresize,curmove
-        mousebind=NONE,btn_middle,togglemaxmizescreen,0
+        mousebind=SUPER,btn_middle,togglefloating,0
         mousebind=SUPER,btn_right,moveresize,curresize
-        mousebind=NONE,btn_left,toggleoverview,-1
-        mousebind=NONE,btn_right,killclient,0
 
-        # Axis Bindings
+        # Axis Bindings (keeping some original functionality)
         axisbind=SUPER,UP,viewtoleft_have_client
         axisbind=SUPER,DOWN,viewtoright_have_client
 
-
         # layer rule
-        layerrule=animation_type_open:zoom,layer_name:rofi
-        layerrule=animation_type_close:zoom,layer_name:rofi
+        layerrule=animation_type_open:zoom,layer_name:wofi
+        layerrule=animation_type_close:zoom,layer_name:wofi
       '';
 
       autostart_sh = ''
