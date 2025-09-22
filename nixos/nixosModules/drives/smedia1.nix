aee21034c3f916711ed9d9e22181d41ef41e67f8
Shringe <git@nsaria.com>
2025-09-22 12:23:17 -0500

    added scrubbing for the rest of the devices

--- nixos/nixosModules/drives/smedia1.nix
+++ nixos/nixosModules/drives/smedia1.nix
@@ -1,37 +1,40 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 with lib;
 let
   cfg = config.nixosModules.drives.smedia1;
-
-  mkMount = extraOpts: {
-    device = "/dev/disk/by-label/smedia1";
-    fsType = "btrfs";
-    options = [ "compress=zstd" "noatime" "nofail" ] ++ extraOpts;
-  };
+  util = import ./util.nix { inherit config pkgs; };
 in
 {
   options.nixosModules.drives.smedia1 = {
     enable = mkEnableOption "first server drive";
   };
 
-  config = mkIf cfg.enable {
-    systemd.tmpfiles.rules = [
-      "d /mnt/btr/pool/smedia1 0755 root root -"
-    ];
+  config = mkIf cfg.enable (mkMerge [
+    {
+      systemd.tmpfiles.rules = [
+        "d /mnt/btr/pool/smedia1 0755 root root -"
+      ];
 
-    fileSystems  = {
-      "/mnt/btr/pool/smedia1" = mkMount [];
-      # "/mnt/server/local" = mkMount [ "subvol=_active/local"];
-    };
+      fileSystems = {
+        "/mnt/btr/pool/smedia1" = util.mkMount "smedia1" [ ];
+        # "/mnt/server/local" = mkMount [ "subvol=_active/local"];
+      };
 
-    services.btrbk.instances = mkIf config.nixosModules.backups.btrbk.enable {
-      "daily".settings.volume."/mnt/btr/pool/smedia1" = {
-        subvolume = {
-          # "_active/backups" = {};
-          "_active/local" = {};
-          "_active/critical" = {};
+      services.btrbk.instances = mkIf config.nixosModules.backups.btrbk.enable {
+        "daily".settings.volume."/mnt/btr/pool/smedia1" = {
+          subvolume = {
+            # "_active/backups" = {};
+            "_active/local" = { };
+            "_active/critical" = { };
+          };
         };
       };
-    };
-  };
+    }
+    (util.mkWeeklyScrub "smedia1" "Thu")
+  ]);
 }
--- nixos/nixosModules/drives/steamssd2.nix
+++ nixos/nixosModules/drives/steamssd2.nix
@@ -1,48 +1,52 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 with lib;
 let
   cfg = config.nixosModules.drives.steamssd2;
-
-  mkMount = extraOpts: {
-    device = "/dev/disk/by-label/steamssd2";
-    fsType = "btrfs";
-    options = [ "compress=zstd" "noatime" "nofail" ] ++ extraOpts;
-  };
-in {
+  util = import ./util.nix { inherit config pkgs; };
+in
+{
   options.nixosModules.drives.steamssd2 = {
     enable = mkEnableOption "second steam ssd";
   };
 
-  config = mkIf cfg.enable {
-    systemd.tmpfiles.rules = [
-      "d /mnt/Steam 0755 root root -"
-      "d /mnt/Steam/libraries 0755 root root -"
+  config = mkIf cfg.enable (mkMerge [
+    {
+      systemd.tmpfiles.rules = [
+        "d /mnt/Steam 0755 root root -"
+        "d /mnt/Steam/libraries 0755 root root -"
 
-      "d /mnt/Steam/libraries/SSD2 0755 root root -"
-      "d /mnt/Steam/Main 0755 root root -"
-      "d /mnt/Saves 0755 root root -"
-      "d /mnt/Steam/Saves 0755 root root -"
-      "d /mnt/Steam/Emulation 0755 root root -"
-    ];
+        "d /mnt/Steam/libraries/SSD2 0755 root root -"
+        "d /mnt/Steam/Main 0755 root root -"
+        "d /mnt/Saves 0755 root root -"
+        "d /mnt/Steam/Saves 0755 root root -"
+        "d /mnt/Steam/Emulation 0755 root root -"
+      ];
 
-    fileSystems = { 
-      "/mnt/btr/pool/steamssd2" = mkMount [];
-      "/mnt/Steam/libraries/SSD2" = mkMount [ "subvol=_active/library" ];
-      "/mnt/Steam/Main" = mkMount [ "subvol=_active/main" ];
-      "/mnt/Saves" = mkMount [ "subvol=_active/saves" ];
-      "/mnt/Steam/Saves" = mkMount [ "subvol=_active/saves" ];
-      "/mnt/Steam/Emulation" = mkMount [ "subvol=_active/emulation" ];
-    };
+      fileSystems = {
+        "/mnt/btr/pool/steamssd2" = util.mkMount "steamssd2" [ ];
+        "/mnt/Steam/libraries/SSD2" = util.mkMount "steamssd2" [ "subvol=_active/library" ];
+        "/mnt/Steam/Main" = util.mkMount "steamssd2" [ "subvol=_active/main" ];
+        "/mnt/Saves" = util.mkMount "steamssd2" [ "subvol=_active/saves" ];
+        "/mnt/Steam/Saves" = util.mkMount "steamssd2" [ "subvol=_active/saves" ];
+        "/mnt/Steam/Emulation" = util.mkMount "steamssd2" [ "subvol=_active/emulation" ];
+      };
 
-    services.btrbk.instances = mkIf config.nixosModules.backups.btrbk.enable {
-      "daily".settings.volume."/mnt/btr/pool/steamssd2" = {
-        subvolume = {
-          "_active/library" = {};
-          "_active/main" = {};
-          "_active/saves" = {};
-          "_active/emulation" = {};
+      services.btrbk.instances = mkIf config.nixosModules.backups.btrbk.enable {
+        "daily".settings.volume."/mnt/btr/pool/steamssd2" = {
+          subvolume = {
+            "_active/library" = { };
+            "_active/main" = { };
+            "_active/saves" = { };
+            "_active/emulation" = { };
+          };
         };
       };
-    };
-  };
+    }
+    (util.mkWeeklyScrub "steamssd2" "Wed")
+  ]);
 }
