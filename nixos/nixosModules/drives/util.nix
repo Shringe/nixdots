03dadb79c3d86aa0316664b9fc2ed83b45a72fc9
Shringe <dashingkoso@gmail.com>
2025-09-22 12:06:42 -0500

    added weekly scrubbing and reporting service to root

---
+++ nixos/nixosModules/drives/util.nix
@@ -0,0 +1,35 @@
+{
+  config,
+  pkgs,
+  ...
+}:
+{
+  mkWeeklyScrub = label: day: {
+    systemd.services."btrfs-scrub-${label}" = {
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = "btrfs scrub start -B /dev/disk/by-label/${label}";
+        ExecStartPost = pkgs.writeShellScript "btrfs-scrub-${label}" ''
+          out=$(${pkgs.btrfs-progs}/bin/btrfs scrub status /dev/disk/by-label/${label})
+          ${config.nixosModules.reporting.matrixReport}/bin/matrixReport "<p>Btrfs scrub report for ${label}:</p><code>$out</code>"
+        '';
+
+        Nice = 19;
+        IOSchedulingClass = 3;
+        IOSchedulingPriority = 7;
+      };
+
+      requisite = [ "local-fs.target" ];
+      after = [ "local-fs.target" ];
+    };
+
+    systemd.timers."btrfs-scrub-${label}" = {
+      wantedBy = [ "timers.target" ];
+      timerConfig = {
+        OnCalendar = "${day} *-*-* 02:00:00";
+        Persistent = true;
+        RandomizedDelaySec = "1h";
+      };
+    };
+  };
+}
