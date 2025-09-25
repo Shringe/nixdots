d629dd0a3da7667bf0da083fa4e015789b5007d9
Shringe <dashingkoso@gmail.com>
2025-09-25 18:57:11 -0500

    updated yanky config

--- home-manager/homeManagerModules/nixvim/extra-plugins/yanky.nix
+++ home-manager/homeManagerModules/nixvim/extra-plugins/yanky.nix
@@ -1,3 +1,17 @@
 {
-  plugins.yanky.enable = true;
+  plugins.telescope.keymaps."<leader>fy" = "yank_history";
+  plugins.yanky = {
+    enable = true;
+    enableTelescope = true;
+
+    settings = {
+      highlight = {
+        on_put = true;
+        on_rank = true;
+        timer = 500;
+      };
+
+      preserve_cursor_position.enabled = true;
+    };
+  };
 }
