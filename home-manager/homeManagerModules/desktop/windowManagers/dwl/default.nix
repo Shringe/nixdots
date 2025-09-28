9027863f6c82c7a28a7938a518c5219e6fadba76
Shringe <dashingkoso@gmail.com>
2025-09-28 18:11:10 -0500

    consilidated wlroots session targets

--- home-manager/homeManagerModules/desktop/windowManagers/dwl/default.nix
+++ home-manager/homeManagerModules/desktop/windowManagers/dwl/default.nix
@@ -45,14 +45,7 @@ in
       swayosd.enable = true;
       dolphin.enable = true;
       polkit.enable = true;
-    };
-
-    systemd.user.targets.dwl-session.Unit = {
-      Description = "dwl compositor session";
-      Documentation = [ "man:systemd.special(7)" ];
-      BindsTo = [ "graphical-session.target" ];
-      Wants = [ "graphical-session-pre.target" ];
-      After = [ "graphical-session-pre.target" ];
+      systemd.enable = true;
     };
   };
 }
---
+++ home-manager/homeManagerModules/desktop/windowManagers/utils/systemd.nix
@@ -0,0 +1,23 @@
+{ config, lib, ... }:
+with lib;
+let
+  cfg = config.homeManagerModules.desktop.windowManagers.utils.systemd;
+in
+{
+  options.homeManagerModules.desktop.windowManagers.utils.systemd = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+    };
+  };
+
+  config = mkIf cfg.enable {
+    systemd.user.targets.wlroots-session.Unit = {
+      Description = "wlroots compositor session";
+      Documentation = [ "man:systemd.special(7)" ];
+      BindsTo = [ "graphical-session.target" ];
+      Wants = [ "graphical-session-pre.target" ];
+      After = [ "graphical-session-pre.target" ];
+    };
+  };
+}
