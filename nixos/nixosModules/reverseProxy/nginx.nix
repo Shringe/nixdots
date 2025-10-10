62ffcf52d675d1ad11b73f2ebefc02e8bb7a9d39
Shringe <git@nsaria.com>
2025-10-10 12:42:28 -0500

    basic searx setup

--- nixos/nixosModules/reverseProxy/nginx.nix
+++ nixos/nixosModules/reverseProxy/nginx.nix
@@ -96,6 +96,7 @@ in
           (rps "router" server.router)
           (rps "ssh" server.ssh)
           (rps "paperless" server.services.paperless)
+          # (rps "searxng" server.services.searxng)
 
           # Public services
           (rpad social.matrix.conduit)
