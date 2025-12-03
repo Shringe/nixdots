07398db242a08c960dcbba15d86943972246e86c
Shringe <dashingkoso@gmail.com>
2025-12-02 19:53:58 -0600

    gatus add homepage

--- nixos/nixosModules/monitors/gatus.nix
+++ nixos/nixosModules/monitors/gatus.nix
@@ -59,52 +59,56 @@ in
       enable = true;
       environmentFile = config.sops.secrets."gatus".path;
 
-      settings = with config.nixosModules; {
-        web.port = cfg.port;
+      settings =
+        with config.nixosModules;
+        with config.nixosModules.server.services;
+        {
+          web.port = cfg.port;
 
-        alerting.matrix = {
-          server-url = config.nixosModules.social.matrix.conduit.furl;
-          access-token = "\${MATRIX_ACCESS_TOKEN}";
-          internal-room-id = "!E5MUdlKUbI2XBDCWXu:${config.nixosModules.reverseProxy.aDomain}";
+          alerting.matrix = {
+            server-url = config.nixosModules.social.matrix.conduit.furl;
+            access-token = "\${MATRIX_ACCESS_TOKEN}";
+            internal-room-id = "!E5MUdlKUbI2XBDCWXu:${config.nixosModules.reverseProxy.aDomain}";
+          };
+
+          endpoints = flatten [
+            (mkMonitor "Radicale" caldav.radicale)
+            (mkMonitor "Jellyfin" jellyfin.server)
+            (mkMonitor "File Browser" filebrowser)
+            (mkMonitor "Uptime Kuma" uptimeKuma)
+            (mkMonitor "Gatus" monitors.gatus)
+            (mkMonitor "Guacamole" guacamole)
+            (mkMonitor "AdGuard Home" server.networking.dns)
+            (mkMonitor "Jellyseerr" jellyfin.jellyseerr)
+            (mkMonitor "Ombi" ombi)
+            (mkMonitor "Kavita" kavita)
+            (mkMonitor "Tandoor" groceries.tandoor)
+            (mkMonitor "Immich" immich)
+            (mkMonitor "Ollama" llm.ollama.webui)
+
+            (mkMonitor "Automatic Ripping Machine" docker.automaticrippingmachine)
+            (mkMonitor "Lidarr" arrs.lidarr)
+            (mkMonitor "Radarr" arrs.radarr)
+            (mkMonitor "Sonarr" arrs.sonarr)
+            (mkMonitor "Prowlarr" arrs.prowlarr)
+            (mkMonitor "qBittorrent" torrent.qbittorrent)
+            (mkMonitor "Authelia" authelia)
+            (mkMonitor "Flaresolverr" arrs.flaresolverr)
+            (mkMonitor "Atuin" shell.atuin.server)
+            (mkMonitor "RomM" docker.romm)
+            (mkMonitor "Linkwarden" linkwarden)
+            (mkMonitor "OurShoppingList" docker.ourshoppinglist)
+            (mkMonitor "Nextcloud" nextcloud)
+            (mkMonitor "Matrix" social.matrix.conduit)
+            (mkMonitor "Traccar" traccar)
+            (mkMonitor "Wallos" docker.wallos)
+            (mkMonitor "Paperless" paperless)
+            (mkMonitor "Collabora" collabora)
+            (mkMonitor "SearXNG" searxng)
+            (mkMonitor "Home Assistant" homeAssistant)
+            (mkMonitor "Homepage" homepage)
+          ];
         };
-
-        endpoints = flatten [
-          (mkMonitor "Radicale" caldav.radicale)
-          (mkMonitor "Jellyfin" jellyfin.server)
-          (mkMonitor "File Browser" filebrowser)
-          (mkMonitor "Uptime Kuma" uptimeKuma)
-          (mkMonitor "Gatus" monitors.gatus)
-          (mkMonitor "Guacamole" guacamole)
-          (mkMonitor "AdGuard Home" server.networking.dns)
-          (mkMonitor "Jellyseerr" jellyfin.jellyseerr)
-          (mkMonitor "Ombi" ombi)
-          (mkMonitor "Kavita" kavita)
-          (mkMonitor "Tandoor" groceries.tandoor)
-          (mkMonitor "Immich" server.services.immich)
-          (mkMonitor "Ollama" llm.ollama.webui)
-
-          (mkMonitor "Automatic Ripping Machine" docker.automaticrippingmachine)
-          (mkMonitor "Lidarr" arrs.lidarr)
-          (mkMonitor "Radarr" arrs.radarr)
-          (mkMonitor "Sonarr" arrs.sonarr)
-          (mkMonitor "Prowlarr" arrs.prowlarr)
-          (mkMonitor "qBittorrent" torrent.qbittorrent)
-          (mkMonitor "Authelia" authelia)
-          (mkMonitor "Flaresolverr" arrs.flaresolverr)
-          (mkMonitor "Atuin" shell.atuin.server)
-          (mkMonitor "RomM" docker.romm)
-          (mkMonitor "Linkwarden" linkwarden)
-          (mkMonitor "OurShoppingList" docker.ourshoppinglist)
-          (mkMonitor "Nextcloud" server.services.nextcloud)
-          (mkMonitor "Matrix" social.matrix.conduit)
-          (mkMonitor "Traccar" server.services.traccar)
-          (mkMonitor "Wallos" docker.wallos)
-          (mkMonitor "Paperless" server.services.paperless)
-          (mkMonitor "Collabora" server.services.collabora)
-          (mkMonitor "SearXNG" server.services.searxng)
-          (mkMonitor "Home Assistant" server.services.homeAssistant)
-        ];
-      };
     };
   };
 }
