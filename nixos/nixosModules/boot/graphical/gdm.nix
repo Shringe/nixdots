120f8e858563303d73fda00f4291719203d565f3
Shringe <dashingkoso@gmail.com>
2025-12-09 09:06:49 -0600

    working gdm setup

---
+++ nixos/nixosModules/boot/graphical/gdm.nix
@@ -0,0 +1,27 @@
+{
+  config,
+  lib,
+  ...
+}:
+with lib;
+let
+  cfg = config.nixosModules.boot.graphical.gdm;
+in
+{
+  options.nixosModules.boot.graphical.gdm = {
+    enable = mkOption {
+      type = types.bool;
+      default = config.nixosModules.boot.graphical.enable;
+      # default = false;
+    };
+  };
+
+  config = mkIf cfg.enable {
+    # stylix.targets.gdm.useWallpaper = false;
+    # programs.gdm.settings.background.path = config.nixosModules.theming.wallpapers.secondary;
+
+    services.displayManager.gdm = {
+      enable = true;
+    };
+  };
+}
