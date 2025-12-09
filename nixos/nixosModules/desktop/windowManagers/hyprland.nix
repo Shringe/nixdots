3397d45820d97623c3f11d65cd34a339795848e9
Shringe <dashingkoso@gmail.com>
2025-12-09 10:15:27 -0600

    general

--- nixos/nixosModules/desktop/windowManagers/hyprland.nix
+++ nixos/nixosModules/desktop/windowManagers/hyprland.nix
@@ -20,9 +20,9 @@ in
   config = mkIf cfg.enable {
     programs.hyprland = {
       enable = true;
-      package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-      portalPackage =
-        inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+      # package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+      # portalPackage =
+      #   inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
     };
 
     environment.sessionVariables.NIXOS_OZONE_WL = "1";
