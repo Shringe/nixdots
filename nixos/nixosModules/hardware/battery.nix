c29c3b214a6b14cace96b164820479db1910c544
Shringe <dashingkoso@gmail.com>
2025-12-10 13:33:42 -0600

    back to tlp

--- nixos/nixosModules/hardware/battery.nix
+++ nixos/nixosModules/hardware/battery.nix
@@ -17,12 +17,12 @@ in
 
     auto-cpufreq = mkOption {
       type = types.bool;
-      default = cfg.enable;
+      default = false;
     };
 
     tlp = mkOption {
       type = types.bool;
-      default = false;
+      default = cfg.enable;
     };
   };
 
@@ -40,16 +40,16 @@ in
       tlp = mkIf cfg.tlp {
         enable = true;
         settings = {
-          CPU_SCALING_GOVERNOR_ON_AC = "powersave";
-          CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+          # CPU_SCALING_GOVERNOR_ON_AC = "powersave";
+          # CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-          CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-          CPU_ENERGY_PERF_POLICY_ON_AC = "power";
+          # CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+          # CPU_ENERGY_PERF_POLICY_ON_AC = "power";
 
-          CPU_MIN_PERF_ON_AC = 0;
-          CPU_MAX_PERF_ON_AC = 100;
-          CPU_MIN_PERF_ON_BAT = 0;
-          CPU_MAX_PERF_ON_BAT = 20;
+          # CPU_MIN_PERF_ON_AC = 0;
+          # CPU_MAX_PERF_ON_AC = 100;
+          # CPU_MIN_PERF_ON_BAT = 0;
+          # CPU_MAX_PERF_ON_BAT = 20;
 
           # Optional helps save long term battery health
           START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
