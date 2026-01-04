0c7f06e543fb4b36d4ff54fb323e5c3dc0da7f86
Shringe <dashingkoso@gmail.com>
2026-01-04 15:34:57 -0600

    reduce snapshots to save space

--- nixos/nixosModules/backups/btrbk.nix
+++ nixos/nixosModules/backups/btrbk.nix
@@ -13,8 +13,8 @@ in
       "btrbk" = {
         onCalendar = "daily";
         settings = {
-          snapshot_preserve_min = "3d";
-          snapshot_preserve = "14d 2w";
+          snapshot_preserve_min = "2d";
+          snapshot_preserve = "7d";
 
           target_preserve_min = "no";
           target_preserve = "4w 2m";
