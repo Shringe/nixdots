c4b0ce564ce5ccf4761ab24482edef05a0f85260
Shringe <dashingkoso@gmail.com>
2026-01-29 13:05:33 -0600

    no java

--- home-manager/homeManagerModules/nixvim/extra-plugins/java.nix
+++ home-manager/homeManagerModules/nixvim/extra-plugins/java.nix
@@ -1,6 +1,6 @@
 {
-  plugins.java.enable = false;
-  performance.combinePlugins.standalonePlugins = [
-    "mason.nvim"
-  ];
+  # plugins.java.enable = false;
+  # performance.combinePlugins.standalonePlugins = [
+  #   "mason.nvim"
+  # ];
 }
