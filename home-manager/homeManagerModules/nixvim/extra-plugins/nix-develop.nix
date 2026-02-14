a9dbec77e357c73176e8400096ca925dcba06569
Shringe <dashingkoso@gmail.com>
2026-02-13 23:42:50 -0600

    enhanced nix-develop vim integration

--- home-manager/homeManagerModules/nixvim/extra-plugins/nix-develop.nix
+++ home-manager/homeManagerModules/nixvim/extra-plugins/nix-develop.nix
@@ -1,3 +1,21 @@
 {
-  plugins.nix-develop.enable = true;
+  plugins.nix-develop = {
+    enable = true;
+
+    luaConfig.post = ''
+      local original_nix_develop = require("nix-develop").nix_develop
+      require("nix-develop").nix_develop = function(args)
+        original_nix_develop(args)
+        vim.defer_fn(vim.cmd.LspRestart, 1000)
+      end
+
+      vim.api.nvim_create_autocmd("VimEnter", {
+        callback = function()
+          if vim.fn.filereadable("flake.nix") == 1 then
+            vim.cmd.NixDevelop()
+          end
+        end,
+      })
+    '';
+  };
 }
