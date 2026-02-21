ddbc4857cbf3695edbb508c108ed3897fb02ad82
Shringe <dashingkoso@gmail.com>
2026-02-21 10:18:22 -0600

    enable kanata restarting

--- nixos/nixosModules/hardware/kanata/default.nix
+++ nixos/nixosModules/hardware/kanata/default.nix
@@ -20,6 +20,7 @@ in
   };
 
   config = mkIf cfg.enable {
+    systemd.services.kanata-default.serviceConfig.Restart = "always";
     services.kanata = {
       enable = true;
       keyboards.default = {
