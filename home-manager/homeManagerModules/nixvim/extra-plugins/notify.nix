c14f7163cbb053934b99e5a2c6864f3e56bed851
Shringe <dashingkoso@gmail.com>
2026-02-23 11:54:16 -0600

    change notification style

--- home-manager/homeManagerModules/nixvim/extra-plugins/notify.nix
+++ home-manager/homeManagerModules/nixvim/extra-plugins/notify.nix
@@ -1,9 +1,13 @@
 {
-  programs.nixvim.plugins.notify = {
+  plugins.notify = {
     enable = true;
+
     settings = {
-      # background_colour = "#000000";
-      style = "minimal";
+      background_colour = "#000000";
+      style = "simple";
+      timeout = 3000;
+      fps = 175;
+      top_down = true;
     };
   };
 }
