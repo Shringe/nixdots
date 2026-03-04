0e1544942efb93f3c5f57efe06b33481061d1e75
Shringe <git@nsaria.com>
2026-03-04 13:26:58 -0600

    switch from niri to hyprland for regreet for better display support

--- nixos/nixosModules/boot/graphical/regreet.nix
+++ nixos/nixosModules/boot/graphical/regreet.nix
@@ -8,20 +8,74 @@ with lib;
 let
   cfg = config.nixosModules.boot.graphical.regreet;
 
-  niri-config = pkgs.writeText "niri-config" ''
-    hotkey-overlay {
-      skip-at-startup
+  hyprland-config = pkgs.writeText "hyprland-config" ''
+    misc {
+      disable_hyprland_logo = true
+      disable_splash_rendering = true
+      disable_hyprland_guiutils_check = true
+      disable_autoreload = true
     }
 
-    environment {
-      GTK_USE_PORTAL "0"
-      GDK_DEBUG "no-portals"
+    monitorv2 {
+      output = eDP-1
+      bitdepth = 10
+      mode = 1920x1080@60
+      position = 0x0
+      scale = 1
+      vrr = 0
     }
 
-    // other settings
+    monitorv2 {
+      output = DP-1
+      disabled = true
+      bitdepth = 10
+      mode = 2560x1440@165
+      position = auto-left
+      scale = 1
+      vrr = 1
+    }
 
-    spawn-at-startup "${pkgs.bash}/bin/bash" "-c" "${pkgs.greetd.regreet}/bin/regreet; ${pkgs.niri}/bin/niri msg action quit --skip-confirmation"
+    monitorv2 {
+      output = HDMI-A-1
+      bitdepth = 10
+      mode = 3440x1440@175
+      position = 0x0
+      scale = 1
+      vrr = 1
+    }
+
+    device {
+      name = msft0001:01-04f3:3138-touchpad
+      sensitivity = 0.000000
+    }
+
+    input {
+      accel_profile = flat
+      follow_mouse = true
+      repeat_delay = 500
+      repeat_rate = 30
+      sensitivity = -0.675000
+    }
+
+    env = GTK_USE_PORTAL,0
+    env = GDK_DEBUG,no-portals
+
+    exec-once = ${pkgs.regreet}/bin/regreet; hyprctl dispatch exit
+    exec-once = hyprctl setcursor "${config.stylix.cursor.name}" ${toString config.stylix.cursor.size}
   '';
+
+  # niri-config = pkgs.writeText "niri-config" ''
+  #   hotkey-overlay {
+  #     skip-at-startup
+  #   }
+  #
+  #   environment {
+  #     GTK_USE_PORTAL "0"
+  #     GDK_DEBUG "no-portals"
+  #   }
+  #
+  #   spawn-at-startup "${pkgs.bash}/bin/bash" "-c" "${pkgs.greetd.regreet}/bin/regreet; ${pkgs.niri}/bin/niri msg action quit --skip-confirmation"
+  # '';
 in
 {
   options.nixosModules.boot.graphical.regreet = {
@@ -40,7 +94,8 @@ in
       enable = true;
       settings = {
         default_session = {
-          command = "${pkgs.niri}/bin/niri -c ${niri-config}";
+          # command = "${pkgs.niri}/bin/niri -c ${niri-config}";
+          command = "/run/current-system/sw/bin/start-hyprland -- --config ${hyprland-config}";
           user = "greeter";
         };
       };
