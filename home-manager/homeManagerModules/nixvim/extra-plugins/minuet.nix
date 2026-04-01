d862d5dc644badeb06c672958b935bd6b0afd064
Shringe <git@nsaria.com>
2026-03-31 20:04:13 -0500

    increase context

--- home-manager/homeManagerModules/nixvim/extra-plugins/minuet.nix
+++ home-manager/homeManagerModules/nixvim/extra-plugins/minuet.nix
@@ -12,14 +12,14 @@ in
       provider = "openai_fim_compatible";
 
       # recommend for local model for resource saving
-      n_completions = 1;
+      n_completions = 2;
 
       # I recommend beginning with a small context window size and incrementally
       # expanding it, depending on your local computing power. A context window
       # of 512, serves as an good starting point to estimate your computing
       # power. Once you have a reliable estimate of your local computing power,
       # you should adjust the context window to a larger value.
-      context_window = 512;
+      context_window = 4096;
 
       provider_options = {
         openai_fim_compatible = {
